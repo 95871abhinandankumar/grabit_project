2bfe7cd48687399ffd35c650bd569160c61ab95e
Abhinandan <abhinandank626@gmail.com>
2022-04-21 01:21:47 +0530

    added requirement.txt file

--- grabit/forms.py
+++ grabit/forms.py
@@ -1,14 +1,18 @@
 from dataclasses import fields
 import email
 from django import forms
-from django.forms import ModelForm, PasswordInput
+
 from .models import User
 from django.core.validators import RegexValidator
 
-class UserForm(ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
         model = User
+        
         fields = ['first_name', 'last_name', 'roll_number', 'room_number', 'phone_number', 'email_id', 'password_field', 'image']
+        widgets={
+            'password_field' : forms.PasswordInput(),
+        }
         error_messages = {
             'email_id': {
                     'Required':"Institute Email-id required"                
